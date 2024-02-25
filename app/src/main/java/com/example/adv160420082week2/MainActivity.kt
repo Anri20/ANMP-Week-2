395af0cdf6d0b78940614762cb7c00604088c67a
Anri20 <88647934+Anri20@users.noreply.github.com>
2024-02-25 16:43:48 +0700

    Add Drawer with Nav Component

--- app/src/main/java/com/example/adv160420082week2/MainActivity.kt
+++ app/src/main/java/com/example/adv160420082week2/MainActivity.kt
@@ -2,21 +2,27 @@ package com.example.adv160420082week2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
+    private lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, findViewById<DrawerLayout>(R.id.drawerLayout))
+
+        val navView = findViewById<NavigationView>(R.id.navView)
+        NavigationUI.setupWithNavController(navView, navController)
 
 //        Menghubungkan bottom nav view dengan NavController
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
@@ -24,6 +30,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp()
+//        return navController.navigateUp()
+
+//        super.onSupportNavigateUp() -> automatically detects if the user is on the top level of
+//        backstack: the drawer icon will show instead of the back button
+        return NavigationUI.navigateUp(navController, findViewById<DrawerLayout>(R.id.drawerLayout)) || super.onSupportNavigateUp()
     }
 }
